2be1f512d5ad209e1016b9991a5b430f30e8789e
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-02-10 11:37:21 -0300

    created mysql DB connection

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-// express import
+/* imports:
+*/
 const express = require('express');
 const app = express();
 
+// database:
+const mysql = require('mysql2');
+const projectDB = mysql.createConnection ( {
+    host: 'localhost',
+    user: 'root',
+    password: '2236',
+    database: 'projeto'
+} )
+
+projectDB.connect( ( error ) => {
+    if ( error ) throw error
+
+    console.log(`Sucessfull connected to "${projectDB.config.database}" database! :)`)
+});
+
 // hello world route
-app.get('/', (req, res) => {
+app.get('/', ( req , res ) => {
     res.write('Learning node and mySQL');
-    res.end()
+    res.end();
+});
+
+app.get('/teste', ( req, res ) => {
+    res.write('Teste page')
 });
 
 // hello world server
 app.listen(8080, () => {
-    console.log('Server running on: http://localhost:8080')
+    console.log('Server running on: http://localhost:8080');
 });
